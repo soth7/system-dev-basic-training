f01af3578bb66ec24cb10983317c34d74fb76614
ikeda-ks <kosuke.ikeda@cleaveware.com>
2025-03-14 10:51:21 +0900

    add day4 work

---
+++ day4/janken/JankenGame.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class JankenGame {
+    public static void main(String[] args) {
+    }
+}
---
+++ day4/janken/Player.java
@@ -0,0 +1,3 @@
+public class Player {
+    // TODO: クラスの中身を実装してください
+}
